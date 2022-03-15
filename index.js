f4e6e5838c26e910a6c2c1dbfbb8d1e11c393c6e
Scott Graham <scttgrhm7+public@gmail.com>
2022-03-14 22:21:23 -0700

    Add port env var

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/7times/:multiplicand', (req, res) => {
   res.send(`7 multiplied by ${multiplicand} is ${result}`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
